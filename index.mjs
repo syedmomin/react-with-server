c96ba8aa006e037745971686c9024b45e2db15b1
momin_finspace <s.momin@finspacegroup.co.uk>
2022-12-21 22:53:02 +0500

    update port

--- index.mjs
+++ index.mjs
@@ -2,7 +2,7 @@ import express from 'express';
 import path from 'path';
 
 const app = express()
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.get('/abc', (req, res) => {
     console.log("request ip: ", req.ip);
