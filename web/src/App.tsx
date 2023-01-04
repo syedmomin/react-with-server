5e67aa514583a8cb83f509b12caa5cede709b567
momin_finspace <s.momin@finspacegroup.co.uk>
2023-01-05 00:05:24 +0500

    sginup form styling

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
 
 function App() {
 
+  const [SignupForm, setSignupForm] = useState(false);
   const autoFun = async () => {
     await axios.post(`http://localhost:5001/registration`, {
       userName: "momin",
@@ -19,8 +20,11 @@ function App() {
       })
   };
 
+  const changeSignup = () => {
+    setSignupForm(true)
+  }
   useEffect(() => {
-    autoFun()
+    // autoFun()
   }, []);
 
   return (
@@ -29,17 +33,36 @@ function App() {
         <div className="shape"></div>
         <div className="shape"></div>
       </div>
-      <form>
-        <h3>Login Your Identity</h3>
+      {!SignupForm &&
+        <form>
+          <h3>Login Form</h3>
 
-        <label>Username :</label>
-        <input type="text" placeholder="Email" />
+          <label>Username :</label>
+          <input type="text" placeholder="Email" />
 
-        <label>Password :</label>
-        <input type="password" placeholder="Password" />
+          <label>Password :</label>
+          <input type="password" placeholder="Password" />
 
-        <button>Log In</button>
-      </form>
+          <button>Log In</button>
+          <p onClick={changeSignup}>Create new account?</p>
+        </form>
+      }
+      {SignupForm &&
+        <form>
+          <h3>Registration</h3>
+
+          <label>Username :</label>
+          <input type="text" placeholder="Username" />
+          <label>Email :</label>
+          <input type="email" placeholder="Email" />
+          <label>Phone :</label>
+          <input type="number" placeholder="Phone" />
+          <label>Password :</label>
+          <input type="password" placeholder="Password" />
+          <button>Sgin Up</button>
+          <p onClick={changeSignup}>Create new account?</p>
+        </form>
+      }
     </>
   );
 }
