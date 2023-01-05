4fe2804031af117b3a01ee6b13871d1b450728d5
Syed Momin <s.momin@finspacegroup.co.uk>
2023-01-05 19:07:31 +0500

    chnages

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from './component/login/login';
+import ResponseModal from './component/modal/responseModal';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
   return (
     <>
-    <Login/>
+    <ResponseModal modalState="true" status="success" response="done hogyia"/>
+    {/* <Login/> */}
     </>
   );
 }
